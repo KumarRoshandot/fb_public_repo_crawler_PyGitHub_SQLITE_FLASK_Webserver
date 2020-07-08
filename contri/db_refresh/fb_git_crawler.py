c57b596afa76ce3afe8b47678e18d21cf2658a85
Kumar Roshan <52309589+KumarRoshandot@users.noreply.github.com>
2020-07-08 23:08:33 +0530

    Create fb_git_crawler.py

---
+++ contri/db_refresh/fb_git_crawler.py
@@ -0,0 +1,74 @@
+from github import Github
+from github.GithubException import GithubException
+from multiprocessing import Pool
+import sys, json, os
+from datetime import datetime
+from contri.db_refresh.db_table_utils import *
+
+user = 'KumarRoshandot'
+password = 'Generation@29'
+org_name = 'facebook'
+
+Client_ID = 'fcf535863e9e6e3daa1a'
+Client_Secret = '5c480bae5d597a7e232b5a853bc974f9981a7d51'
+
+#public_repos_contributors_json = "temp/public_repos_contributors.json"
+#public_repos_commits = "temp/public_repos_commits.json"
+
+
+def crawl_and_insert(org,param,conn):
+    print(param)
+    if param['type'] == 'login':
+        git = Github(param['user'], param['password'])
+    else:
+        git = Github(client_id=param['client_id'],client_secret=param['client_secret'])
+    cur = conn.cursor()
+    print('Starting Fetching data')
+    try:
+        repos = git.get_organization(org).get_repos()
+        for i in repos:
+            try:
+                # print(i.name,i.created_at,i.full_name,i.id)
+                cur.execute("INSERT OR IGNORE into public_repos (id,name,full_name,begin_date) values (?,?,?,?)",
+                                (i.id,i.name,i.full_name,str(i.created_at)))
+                conn.commit()
+
+                # here it can be done better but i am fetching  the max(commit_date) from child table of that repo.
+                # So  that i can perform incremental load on child table,  when next refresh happens
+                cur.execute("select max(commit_dt) as last_commit_dt from public_repos_commits where repo_id = {}".format(i.id))
+                result = cur.fetchone()
+                last_commit_date = datetime.strptime(result[0], '%Y-%m-%d %H:%M:%S')
+
+                for j in i.get_commits(sha='master',since=last_commit_date):
+                    # print(j.sha,j.commit.author.name,j.commit.author.date,j.committer.id,j.committer.name,j.committer.login)
+                    cur.execute("INSERT OR IGNORE into public_repos_commits (commit_id,repo_id,author_name,commit_dt) values (?,?,?,?)",
+                                    (j.sha,i.id,j.commit.author.name,j.commit.author.date))
+                    conn.commit()
+
+            except GithubException as e:
+                # I can USE LOGGER for LOGGING
+                print(e)
+            except Exception as e:
+                print(e)
+        cur.close()
+        conn.close()
+        print('DATABASE TABLE REFRESHED')
+        return 'DATABASE TABLE REFRESHED'
+    except Exception as e:
+        print('EXCEPTION OCCURED :- '+str(e))
+        return 'EXCEPTION OCCURED :- '+str(e)
+
+
+if __name__ == "__main__":
+    #pool = Pool(os.cpu_count())
+    # get connection to db
+    try:
+        user_credentials = {'user':user,'password':password,'type':'login'}
+        user_app_token = {'client_id': Client_ID, 'client_secret': Client_Secret, 'type': 'token'}
+        conn = create_db_tables()
+        response = crawl_and_insert(org_name, user_credentials,conn)
+        print(response)
+    except Exception as e:
+        print(str(e))
+
+
